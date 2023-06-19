c361414cd1546e1cf259013036ea41f09c3a5683
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-06-19 14:49:49 +0530

    Added Exceptions

---
+++ src/main/java/com/example/FlipCommerce/exceptions/InvalidCardException.java
@@ -0,0 +1,7 @@
+package com.example.FlipCommerce.exceptions;
+
+public class InvalidCardException extends Exception{
+    public InvalidCardException(String message){
+        super(message);
+    }
+}
